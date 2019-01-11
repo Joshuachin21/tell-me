c510c561d64b5ba5f8b6acd00587adf774652fa4
Joshua Chin <joshua.chin21@gmail.com>
2019-01-11 00:48:03 -0800

    add api because github namecheck endpoint is dumb and not working. preventing me from creating a new repo.

---
+++ tell-me-api/routes/upload.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var GoogleDriveServices = require('../services/GoogleDriveServices');
+
+/* GET users listing. */
+router.post('/audio', function (req, res, next) {
+    console.log(req.files.foo.name);
+    console.log(req.files.foo.mimetype);
+    let filePath = './tmp/';
+    let fileName = req.files.foo.name;
+    let fileLocation = filePath + fileName;
+    downloadFile(req.files.foo.data, fileLocation);
+    GoogleDriveServices.uploadFile(filePath, fileName);
+    res.send('respond with a resource');
+});
+
+function downloadFile(fileBuffer, fileName) {
+    fs.writeFile(fileName, fileBuffer, (err) => {
+        if (err) throw err;
+
+        console.log("The file was succesfully saved!");
+    });
+}
+
+
+module.exports = router;
