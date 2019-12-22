0365d83fa78856895d0c360ef221fd5630cb4a32
Joshua Chin <joshua.chin21@gmail.com>
2019-12-22 10:38:08 -0800

    second web server

---
+++ tell-me-api-2/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/upload');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+console.log('use inddexrouter');
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
+
+app.listen(3005);
