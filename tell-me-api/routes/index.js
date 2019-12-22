ad56eb49bdb4f39f67f4d5054fe4fa006f904d78
Joshua Chin <joshua.chin21@gmail.com>
2019-12-22 11:20:24 -0800

    merge resolve

--- tell-me-api/routes/index.js
+++ tell-me-api/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('../public/index.html', { title: 'Express' });
+  console.log('index hit');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
